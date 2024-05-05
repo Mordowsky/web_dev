ee5275347cc810b65cf9b782cc7e17ac57ac9d21
Мордовский Алексей <alexeymordowsky@gmail.com>
2024-05-05 18:10:07 +0300

    Добавил формы для tasks и projects

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,43 @@
 from .models import Project, Task
+from .forms import FeedbackForm, ProjectForm, TaskForm
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'tasks/index.html')
 
-# def index2(request):
-#     projects_list_url = reverse('tasks:projects_list')
-#     quality_page_url = reverse('tasks:quality_control_page')
-#     html = f'''<h1>Страница приложения tasks</h1><a href='{projects_list_url}'>Список проектов</a><br>
-#     <a href='{quality_page_url}'>Страница приложения качества</a>'''
-#     return HttpResponse(html)
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            # Обработка данных формы
+            return redirect('/tasks')
+    else:
+        form = FeedbackForm()
+    return render(request, 'tasks/feedback.html', {'form': form})
+
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:projects_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'tasks/project_create.html', {'form': form})
+
+def add_task_to_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            return redirect('tasks:project_detail', project_id=project.id)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form, 'project': project})
 
 def projects_list(request):
     projects = Project.objects.all()
